5fb01110cdddef18eaad0bf75121d9c6a437b71e
Dominik Schidlowski <dominik@schidlowski.eu>
2020-09-18 21:06:21 +0200

    prepare deploy files

--- application/settings.go
+++ application/settings.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"io/ioutil"
 	"os/user"
 )
 
@@ -23,11 +25,26 @@ func initSettings() *Settings {
 
 	loadConfigFile(currentUser.HomeDir)
 
+	if viper.GetString("PostgresPasswordFile") != "" {
+		buffer, err := ioutil.ReadFile(viper.GetString("PostgresPasswordFile"))
+		if err != nil {
+			panic(err)
+		}
+		viper.Set("PostgresPassword", string(buffer))
+	}
+
 	return &Settings{
 		ConversionDestinationDirectory: viper.GetString("ConversionDestinationDirectory"),
 		UploadDestinationDirectory:     viper.GetString("UploadDestinationDirectory"),
 		HttpListenEndpoint:             viper.GetString("HttpListenEndpoint"),
-		Dsn:                            viper.GetString("Dsn"),
+		Dsn: fmt.Sprintf(
+			"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Europe/Berlin",
+			viper.GetString("PostgresHostname"),
+			viper.GetString("PostgresUsername"),
+			viper.GetString("PostgresPassword"),
+			viper.GetString("PostgresDbName"),
+			viper.GetInt32("PostgresPort"),
+		),
 	}
 }
 
@@ -35,10 +52,16 @@ func loadConfigFile(homePath string) {
 	viper.SetDefault("ConversionDestinationDirectory", homePath)
 	viper.SetDefault("UploadDestinationDirectory", homePath)
 	viper.SetDefault("HttpListenEndpoint", "0.0.0.0:8080")
-	viper.SetDefault("Dsn", "host=localhost user=ffmpeg password=ffmpeg dbname=ffmpeg port=9920 sslmode=disable TimeZone=Europe/Berlin")
+	viper.SetDefault("PostgresUsername", "ffmpeg")
+	viper.SetDefault("PostgresHostname", "localhost")
+	viper.SetDefault("PostgresPassword", "ffmpeg")
+	viper.SetDefault("PostgresPasswordFile", "")
+	viper.SetDefault("PostgresDbName", "ffmpeg")
+	viper.SetDefault("PostgresPort", "9920")
 
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
+	viper.SetEnvPrefix("FFMPEG_GO_SERVER_")
 	viper.AddConfigPath("/etc/ffmpeg-server/")
 	if err := viper.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
